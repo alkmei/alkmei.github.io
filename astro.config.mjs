da98ff35571b0c55bbb8a9426d60360cab8538be
Alvin Mei <alvinkmei@gmail.com>
2025-08-27 22:07:46 -0400

    remake

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import react from '@astrojs/react';
-
-import tailwind from '@astrojs/tailwind';
-
 // https://astro.build/config
-export default defineConfig({
-  site: "https://alkmei.github.io",
-  integrations: [react(), tailwind({applyBaseStyles: false,})]
-});
+export default defineConfig({});
